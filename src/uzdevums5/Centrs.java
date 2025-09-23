6c2641a26c20b6363658d7ea2a845fe9caddb918
“sams7891” <samietisrodrigo@gmail.com>
2025-09-23 11:35:22 +0300

    Pabeigta otrā klase(četrstūris)

--- src/uzdevums5/Centrs.java
+++ src/uzdevums5/Centrs.java
@@ -1,5 +1,22 @@
 package uzdevums5;
 
 public class Centrs {
-
+	//Atribūti
+	public int x, y;
+	
+	//konstruktors
+	public Centrs(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//Metodes
+	public String izvaditKoordinatas() {
+		return "Centra punkts atrodas:\n x = " + x + " y = " + y;
+	}
+	
+	public void parvietot(int koordX, int koordY) {
+		x += koordX;
+		y += koordY;
+	}
 }
---
+++ src/uzdevums5/Cetrsturis.java
@@ -0,0 +1,27 @@
+package uzdevums5;
+
+public class Cetrsturis {
+	public double platums, augstums;
+	public Centrs punkts;
+
+//konstruktora pārslogošana
+	public Cetrsturis() {
+		punkts = new Centrs(0, 0);
+	}
+
+	public Cetrsturis(Centrs punkts, double p, double a) {
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+	}
+	
+	public String izvaditCetrsturaInfo() {
+		return "PlatumsL: " + platums + "cm augstums: " + augstums + "cm";
+	}
+	
+	//Uztaisīt metodi kas aprēkina laukumu
+	
+	public double rekinat(double platums, double augstums) {
+		return platums * augstums;
+	}
+}
